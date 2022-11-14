17e2ed0bf1de6fbaf62b9219f34ecd601a3d44d7
Michael East <michael@michaeleast.dev>
2022-11-14 11:24:08 +0000

    Added the structure of the application, which includes the creation of the component structure. Installed styled-components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
+import MainContentContainer from './components/MainContentContainer/MainContentContainer';
+
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledMain>
+      <Header />
+      <SearchBar />
+      <MainContentContainer />
+    </StyledMain>
   );
 }
 
---
+++ src/components/MainContentContainer/MainContentContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainContentContainer() {
+  return <div>MainContentContainer</div>;
+}
+
+export default MainContentContainer;
